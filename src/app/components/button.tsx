ec0673d76270f7ee68979bd01b1d3ac230493c73
adricaba06 <caballero.toadr24@triana.salesianos.edu>
2025-02-08 12:47:15 +0100

    He añadido un boton

---
+++ src/app/components/button.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// Definir la interfaz para las props
+
+//Primero vamos a crear una interface que defina las properties del boton
+
+interface ButtonProps {
+
+    contenido: string; //texto que va a tener dentro el boton
+    onClick: () => void; //es una funcion por lo cual debo de poner asi la sintaxis
+}
+
+//una vez creada la interfaz vamos a declarar el componente
+
+export default function Button({contenido, onClick}:ButtonProps){ //recuerda decir el tipo, TypeScript es titismiquis
+    return(
+        <button onClick={onClick} className='boton'>
+            {contenido}
+        </button>
+    );
+}
+
