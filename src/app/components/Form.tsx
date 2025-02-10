91f5a09b088315dde978fdf29705ed729fbf7d50
adricaba06 <caballero.toadr24@triana.salesianos.edu>
2025-02-10 10:02:14 +0100

    Formulario añadido

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,50 +1,48 @@
-import { title } from "process"
 import { useState } from "react"
-
 import React from 'react'
 
 export default function Form() {
 
-    const [values, setValues] = useState({
-        title: '',
-        description: '',
+    const [values, setValues] = useState({ //en vez de poner dos useSte hemos puesto solo uno
+        title: "", //valores iniciales
+        description: ""
     });
 
-    const handleInpunt = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>{ //he tenido que buscar esto en internet
-        const {name, value} = event.target;
+    const handleInpunt = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {name, value} = event.target; //los nuevos valores serán los escritos en el input
+
         setValues({
-            ...values,
-            [name]: value,
+            ...values, //esto hace que se mantangan los valores anteriores, es un poco lioso de entender
+            [name]: value //se rescriben los valores nuevos
         });
+
     }
 
-    const handleForm = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>{
-        event.preventDefault();
-        console.log(values);
-    }
+    const handleForm = (event: React.ChangeEvent<HTMLInputElement>) => event.preventDefault(); //la página no se refrescará
 
   return (
+
     <form>
         <input
         type="text"
         name="title"
-        value = {values.title}
+        value={values.title}
+        onChange={handleInpunt}
         placeholder="Task Name"
-        onChange={handleInpunt}>
-
-        </input>
+        />
 
         <input
         type="text"
         name="description"
-        value = {values.description}
-        placeholder="Description..."
-        onChange={handleInpunt}>
+        value={values.description}
+        onChange={handleInpunt}
+        placeholder="Add a Description"
+        />
 
-        </input>
-
-
-
+        <button>
+            submit
+        </button>
     </form>
+    
   )
 }
