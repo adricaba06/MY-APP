7c877accd7b2f07fae8bbf762d1623eb60fce882
adricaba06 <caballero.toadr24@triana.salesianos.edu>
2025-02-13 14:53:55 +0100

    correjido

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import Button from "./components/button";  
+import Button from "./components/button";
 import Form from "./components/Form";
-import { TaskComponent } from "./components/task";     
+import { TaskComponent } from "./components/task";
 import PopUp from "./components/popUp";
 import { useState, useEffect } from "react";
 import { Task } from "./components/task";
@@ -15,35 +15,49 @@ export default function Home() {
       title: "Tarea de ejemplo",
       description: "Esta es una tarea por defecto",
       selecionada: false,
-      done: false
-    }
+      done: false,
+    },
   ]);
 
-  const addTask = (title: string, description: string, selecionada: boolean) => {
+  const addTask = (
+    title: string,
+    description: string,
+    selecionada: boolean
+  ) => {
     const newTask: Task = {
       id: uuidv4(),
       title,
       description,
       selecionada,
-      done: false,  
+      done: false,
     };
 
     setTaskList([...taskList, newTask]);
   };
 
   const deleteTask = () => {
-    setTaskList(taskList.filter(task => !task.selecionada));
+    setTaskList(taskList.filter((task) => !task.selecionada));
   };
 
-  let modifyCurrentTask = (id: string, newTitle: string, newDescription: string) => {
+  let modifyCurrentTask = (
+    id: string,
+    newTitle: string,
+    newDescription: string
+  ) => {
     setTaskList(
-      taskList.map(task => task.id === id
-        ? { ...task, title: newTitle, description: newDescription } : task
+      taskList.map((task) =>
+        task.id === id
+          ? { ...task, title: newTitle, description: newDescription }
+          : task
       )
     );
   };
 
-  const handlemodification = (id: string, newTitle: string, newDescription: string) => {
+  const handlemodification = (
+    id: string,
+    newTitle: string,
+    newDescription: string
+  ) => {
     changevispop2(); // Abre el PopUp de edición
     setCurrentTask({ id, title: newTitle, description: newDescription }); // Asigna los valores a los inputs
   };
@@ -56,7 +70,7 @@ export default function Home() {
       }
       const updatedTaskList = [
         ...taskList.slice(0, indice),
-        ...taskList.slice(indice + 1)
+        ...taskList.slice(indice + 1),
       ];
 
       setTaskList(updatedTaskList);
@@ -64,15 +78,17 @@ export default function Home() {
   };
 
   const toggleDone = (id: string) => {
-    setTaskList(taskList.map(task =>
-      task.id === id ? { ...task, done: !task.done } : task
-    ));
+    setTaskList(
+      taskList.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : task
+      )
+    );
   };
 
   enum Filter {
     All,
     Done,
-    NotDone
+    NotDone,
   }
 
   const [filter, setFilter] = useState<Filter>(Filter.All);
@@ -82,9 +98,9 @@ export default function Home() {
       case Filter.All:
         return tasks;
       case Filter.Done:
-        return tasks.filter(task => task.done === true);
+        return tasks.filter((task) => task.done === true);
       case Filter.NotDone:
-        return tasks.filter(task => task.done === false);
+        return tasks.filter((task) => task.done === false);
       default:
         throw new Error("Invalid filter");
     }
@@ -96,20 +112,25 @@ export default function Home() {
     setFilteredTasks(filterList(taskList, filter));
   }, [taskList, filter]);
 
-  const showList = () => filteredTasks.map(task => (
-    <TaskComponent
-      key={task.id}
-      {...task}
-      changeSelect={id =>
-        setTaskList(taskList.map(task =>
-          task.id === id ? { ...task, selecionada: !task.selecionada } : task
-        ))
-      }
-      toggleDone={toggleDone}
-      remove={id => deleteCurrentTask(id)}
-      modify={id => handlemodification(id, task.title, task.description)} // Aqui paso la tarea para modificar
-    />
-  ));
+  const showList = () =>
+    filteredTasks.map((task) => (
+      <TaskComponent
+        key={task.id}
+        {...task}
+        changeSelect={(id) =>
+          setTaskList(
+            taskList.map((task) =>
+              task.id === id
+                ? { ...task, selecionada: !task.selecionada }
+                : task
+            )
+          )
+        }
+        toggleDone={toggleDone}
+        remove={(id) => deleteCurrentTask(id)}
+        modify={(id) => handlemodification(id, task.title, task.description)} // Aqui paso la tarea para modificar
+      />
+    ));
 
   const [vispop2, setvispop2] = useState(false);
   const changevispop2 = () => setvispop2(!vispop2);
@@ -118,10 +139,14 @@ export default function Home() {
   const changeVisibility = () => setVisible(!visible);
 
   //ESTADO PARA EL PUÑETERO FORMULARIO
-  const [currentTask, setCurrentTask] = useState<{ id: string, title: string, description: string }>({
-    id: '',
-    title: '',
-    description: ''
+  const [currentTask, setCurrentTask] = useState<{
+    id: string;
+    title: string;
+    description: string;
+  }>({
+    id: "",
+    title: "",
+    description: "",
   });
 
   const handleSubmit = (title: string, description: string) => {
@@ -130,8 +155,8 @@ export default function Home() {
   };
 
   const handleEditSubmit = (title: string, description: string) => {
-    modifyCurrentTask(currentTask.id, title, description); 
-    changevispop2(); 
+    modifyCurrentTask(currentTask.id, title, description);
+    changevispop2();
   };
 
   return (
@@ -141,15 +166,34 @@ export default function Home() {
           <div className="contenidoPrincipal">
             <aside>
               <div>
-                <div className="filtros">
+                <div className="principales">
                   <Button onClick={() => changeVisibility()}>
-                    <p>New Task</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                    New Task{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-plus-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
                       <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
                     </svg>
                   </Button>
 
-                  <Button onClick={deleteTask}>Delete Task</Button>
+                  <Button onClick={deleteTask}>
+                    Delete Task{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-trash3-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                    </svg>
+                  </Button>
                 </div>
 
                 {/* PopUp para agregar tarea */}
@@ -157,23 +201,45 @@ export default function Home() {
                   <Form submit={handleSubmit} />
                 </PopUp>
 
-                {/* PopUp para modificar tarea, tiene formulario normal, no el mio */} 
-                <PopUp isVisible={vispop2}> {/*he visto esto en un video*/}
-                  <form onSubmit={(e) => { e.preventDefault(); handleEditSubmit(currentTask.title, currentTask.description); }}>
-                    <input 
-                      type="text" 
+                {/* PopUp para modificar tarea, tiene formulario normal, no el mio */}
+                <PopUp isVisible={vispop2}>
+                  {" "}
+                  {/*he visto esto en un video*/}
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      handleEditSubmit(
+                        currentTask.title,
+                        currentTask.description
+                      );
+                    }}
+                  >
+                    <input
+                      type="text"
                       required
                       maxLength={16}
-                      value={currentTask.title} 
-                      onChange={(e) => setCurrentTask({ ...currentTask, title: e.target.value })} 
+                      value={currentTask.title}
+                      onChange={(e) =>
+                        setCurrentTask({
+                          ...currentTask,
+                          title: e.target.value,
+                        })
+                      }
                     />
-                    <input 
-                      type="text" 
+                    <input
+                      type="text"
                       maxLength={94}
-                      value={currentTask.description} 
-                      onChange={(e) => setCurrentTask({ ...currentTask, description: e.target.value })} 
+                      value={currentTask.description}
+                      onChange={(e) =>
+                        setCurrentTask({
+                          ...currentTask,
+                          description: e.target.value,
+                        })
+                      }
                     />
-                    <button type="submit" className="Form-button">Save Changes</button>
+                    <button type="submit" className="Form-button">
+                      Save Changes
+                    </button>
                   </form>
                 </PopUp>
 
@@ -181,21 +247,23 @@ export default function Home() {
                 <div className="filtros">
                   <Button onClick={() => setFilter(Filter.All)}>All</Button>
                   <Button onClick={() => setFilter(Filter.Done)}>Done</Button>
-                  <Button onClick={() => setFilter(Filter.NotDone)}>To Do</Button>
+                  <Button onClick={() => setFilter(Filter.NotDone)}>
+                    To Do
+                  </Button>
                 </div>
               </div>
             </aside>
 
             <section>
               <h1>Task Manager</h1>
-              <div className="recuadroTareas">
-                {showList()}
-              </div>
+              <div className="recuadroTareas">{showList()}</div>
             </section>
           </div>
         </main>
         <footer className="footer">
-          <p>© 2025 Adrián Caballero Torrebejano. Todos los derechos reservados.</p>
+          <p>
+            © 2025 Adrián Caballero Torrebejano. Todos los derechos reservados.
+          </p>
         </footer>
       </div>
     </>
